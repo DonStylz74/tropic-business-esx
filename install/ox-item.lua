59d872bff7677038988ba8882154f40a7ff35b11
DonStylz74 <84778838+DonStylz74@users.noreply.github.com>
2025-06-08 13:39:16 +1000

    Add files via upload

---
+++ install/ox-item.lua
@@ -0,0 +1,12 @@
+	["business_license"] = {
+		label = "Business License",
+		weight = 2,
+		stack = false,
+		close = false,
+		description = "License to own a Business",
+		client = {
+			image = "business_license.png",
+		},
+	},
+
+
